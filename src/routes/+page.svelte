8b15669bcbc8dd35756f7ddc7de15abf974a3044
JenChieh <jcs090218@gmail.com>
2024-06-03 04:02:04 -0700

    update page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
 
+  var show = false;
 
+  onMount(() => {
+    var eye = document.getElementById("eye");
+    disable(); // disabled by default
+
+    //var fileds = document.getElementsByTagName();
+
+    eye?.addEventListener("click", function () {
+      show = !show;
+
+      if (show) {
+        enable();
+      } else {
+        disable();
+      }
+    });
+
+    function enable() {
+      eye?.classList.add("no-grayscale");
+      eye?.classList.remove("grayscale");
+    }
+
+    function disable() {
+      eye?.classList.add("grayscale");
+      eye?.classList.remove("no-grayscale");
+    }
+  });
+</script>
+
+<main>
+  <img id="eye" src="./logo.png" alt="eye" width="100px" />
+</main>
+
+<style>
+  :global(.no-grayscale) {
+    filter: grayscale(0%);
+    transition: all 0.8s ease;
+  }
+
+  :global(.grayscale) {
+    filter: grayscale(100%);
+    transition: all 0.8s ease;
+  }
+</style>
