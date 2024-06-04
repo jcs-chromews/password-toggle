be79c0bda37098ad2664878613f2cd79b4202fe1
JenChieh <jcs090218@gmail.com>
2024-06-03 18:16:43 -0700

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,14 @@
 <script>
   import { onMount } from "svelte";
 
-  var show = false;
+  const query = { active: true, currentWindow: true };
 
   onMount(() => {
-    var inputs = document.getElementsByTagName("input");
-    var fields = [];
-    for (let i = 0; i < inputs.length; ++i) {
-      if (inputs[i].type == "password") {
-        fields.push(inputs[i]);
-      }
-    }
+    var show = false;
 
     var eye = document.getElementById("eye");
     disable(); // disabled by default
     var count = document.getElementById("count");
-
-    count.innerHTML = fields.length;
 
     eye?.addEventListener("click", function () {
       show = !show;
@@ -32,19 +24,48 @@
       eye?.classList.add("no-grayscale");
       eye?.classList.remove("grayscale");
 
-      for (let i = 0; i < fields.length; ++i) {
-        fields[i].type = "";
-      }
+      chrome.tabs.query(query, function (tab) {
+        chrome.scripting
+          .executeScript({
+            target: { tabId: tab[0].id },
+            func: () => {
+              var inputs = document.getElementsByTagName("input");
+
+              for (let i = 0; i < inputs.length; ++i) {
+                if (inputs[i].type == "password") {
+                  inputs[i].type = "text";
+                  inputs[i].setAttribute("password-toggle", "mark");
+                }
+              }
+            },
+          })
+          .then(() => console.log("[password-toggle] show password"));
+      });
     }
 
     function disable() {
       eye?.classList.add("grayscale");
       eye?.classList.remove("no-grayscale");
 
-      for (let i = 0; i < fields.length; ++i) {
-        fields[i].type = "password";
-      }
+      chrome.tabs.query(query, function (tab) {
+        chrome.scripting
+          .executeScript({
+            target: { tabId: tab[0].id },
+            func: () => {
+              var inputs = document.getElementsByTagName("input");
+
+              for (let i = 0; i < inputs.length; ++i) {
+                if (inputs[i].getAttribute("password-toggle") == "mark") {
+                  inputs[i].type = "password";
+                }
+              }
+            },
+          })
+          .then(() => console.log("[password-toggle] hide password"));
+      });
     }
+
+    //count.innerHTML = inner_count;
   });
 </script>
 
@@ -62,12 +83,12 @@
 
   :global(.no-grayscale) {
     filter: grayscale(0%);
-    transition: all 0.8s ease;
+    transition: all 0.4s ease;
   }
 
   :global(.grayscale) {
     filter: grayscale(100%);
-    transition: all 0.8s ease;
+    transition: all 0.4s ease;
   }
 
   #count {
